d9b631c3e5551830baf2e47fd1556e6a102b66c4
Benjamin Gautier O <benjamin.gautier.14@sansano.usm.cl>
2018-12-13 00:26:31 -0300

    welcome works properly, swipe views and activity, ready and checked to persistance information

--- app/src/main/java/cl/bgautier/claptrack/ui/platformActivity.java
+++ app/src/main/java/cl/bgautier/claptrack/ui/platformActivity.java
@@ -7,8 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import cl.bgautier.claptrack.R;
 
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,7 +19,9 @@ import android.widget.Toast;
 
 import com.mikepenz.materialdrawer.Drawer;
 
-public class platformActivity extends AppCompatActivity
+import java.util.HashMap;
+
+public class platformActivity extends AppCompatActivity implements GestureDetector.OnGestureListener
 {
     private Button btn_play4;
     private Button btn_play3;
@@ -33,6 +37,10 @@ public class platformActivity extends AppCompatActivity
     private ImageView im_pc;
     private Toolbar mToolbar;
     private Drawer drawer;
+    private GestureDetector gestureDetector;
+
+
+    private HashMap<String,Integer> elected;
 
     private void obtainPlatormsViews() {
         btn_play4 = findViewById(R.id.btn_play4);
@@ -64,20 +72,22 @@ public class platformActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.favourites_platforms);
 
+
+        elected = new HashMap<>();
+        elected.put("play3",0);
+        elected.put("play4",0);
+        elected.put("nintendo_switch",0);
+        elected.put("nintendo_3ds",0);
+        elected.put("xbox_one",0);
+        elected.put("pc",0);
+
         //getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         //getSupportActionBar().setCustomView(R.menu.action_bar);
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         //TextView aa= findViewById(R.id.title_menu);
         //aa.setText("Select Yours Platforms");
 
-        findViewById(R.id.btn_finalize_welcome).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), CardStackActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        gestureDetector = new GestureDetector(platformActivity.this, platformActivity.this);
 
         obtainPlatormsViews();
         setOnclickAll();
@@ -96,6 +106,7 @@ public class platformActivity extends AppCompatActivity
         //drawer = DrawerUtil.getDrawer(this, mToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
     }
 
     @Override
@@ -111,6 +122,7 @@ public class platformActivity extends AppCompatActivity
             @Override
             public void onClick(View v) {
                 btn_play4.setVisibility(View.VISIBLE);
+                elected.put("play4",1);
                 Toast.makeText(getBaseContext(),"lo presionaste", Toast.LENGTH_LONG);
             }
         });
@@ -118,12 +130,14 @@ public class platformActivity extends AppCompatActivity
             @Override
             public void onClick(View v) {
                 btn_play4.setVisibility(View.INVISIBLE);
+                elected.put("play4",0);
             }
         });
         im_play3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 btn_play3.setVisibility(View.VISIBLE);
+                elected.put("play3",1);
                 Toast.makeText(getBaseContext(),"lo presionaste", Toast.LENGTH_LONG);
             }
         });
@@ -131,12 +145,14 @@ public class platformActivity extends AppCompatActivity
             @Override
             public void onClick(View v) {
                 btn_play3.setVisibility(View.INVISIBLE);
+                elected.put("play3",0);
             }
         });
         im_nswitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 btn_nswitch.setVisibility(View.VISIBLE);
+                elected.put("nintendo_switch",1);
                 Toast.makeText(getBaseContext(),"lo presionaste", Toast.LENGTH_LONG);
             }
         });
@@ -144,12 +160,14 @@ public class platformActivity extends AppCompatActivity
             @Override
             public void onClick(View v) {
                 btn_nswitch.setVisibility(View.INVISIBLE);
+                elected.put("nintendo_switch",0);
             }
         });
         im_threds.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 btn_threds.setVisibility(View.VISIBLE);
+                elected.put("nintendo_3ds",1);
                 Toast.makeText(getBaseContext(),"lo presionaste", Toast.LENGTH_LONG);
             }
         });
@@ -157,12 +175,14 @@ public class platformActivity extends AppCompatActivity
             @Override
             public void onClick(View v) {
                 btn_threds.setVisibility(View.INVISIBLE);
+                elected.put("nintendo_3ds",0);
             }
         });
         im_one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 btn_one.setVisibility(View.VISIBLE);
+                elected.put("xbox_one",1);
                 Toast.makeText(getBaseContext(),"lo presionaste", Toast.LENGTH_LONG);
             }
         });
@@ -170,12 +190,14 @@ public class platformActivity extends AppCompatActivity
             @Override
             public void onClick(View v) {
                 btn_one.setVisibility(View.INVISIBLE);
+                elected.put("xbox_one",0);
             }
         });
         im_pc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 btn_pc.setVisibility(View.VISIBLE);
+                elected.put("pc",1);
                 Toast.makeText(getBaseContext(),"lo presionaste", Toast.LENGTH_LONG);
             }
         });
@@ -183,8 +205,88 @@ public class platformActivity extends AppCompatActivity
             @Override
             public void onClick(View v) {
                 btn_pc.setVisibility(View.INVISIBLE);
+                elected.put("pc",0);
             }
         });
 
     }
+
+
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent1, MotionEvent motionEvent2, float X, float Y) {
+        if (motionEvent1.getY() - motionEvent2.getY() > 50) {
+            //Toast.makeText(MainActivity.this, "You Swiped up!", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        if (motionEvent2.getY() - motionEvent1.getY() > 50) {
+            //Toast.makeText(MainActivity.this, "You Swiped Down!", Toast.LENGTH_LONG).show();
+            //Toast.makeText(MainActivity.this, "You Swiped Left!", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, categoryActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+
+        if (motionEvent1.getX() - motionEvent2.getX() > 20 ){
+            Toast.makeText(platformActivity.this, "You Swiped Left!", Toast.LENGTH_LONG).show();
+            //aqui es necesario guardar los elegidos, estan en hashmaps elected
+            Intent intent = new Intent(this, CardStackActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+
+        if (motionEvent2.getX() - motionEvent1.getX() > 20) {
+            Toast.makeText(platformActivity.this, "You Swiped Right!", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, categoryActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onLongPress(MotionEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent arg0, MotionEvent arg1, float arg2, float arg3) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        // TODO Auto-generated method stub
+        return gestureDetector.onTouchEvent(motionEvent);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent e)
+    {
+        super.dispatchTouchEvent(e);
+        return gestureDetector.onTouchEvent(e);
+    }
+
 }
